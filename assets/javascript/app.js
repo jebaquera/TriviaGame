f08ee78505f754c1e5bd2784f33b5bed9e7be67d
jebaquera <jebaquera@gmail.com>
2019-06-05 09:56:32 -0500

    added script and pseudocode text.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,9 +1,21 @@
 $(document).ready(function(){
 
+// Game starts with background [display background art, logo art, instruction text] and Start screen [button]
 
+// When player clicks Start, the Main Text screen displays [Show titling, time remaining stopwatch, and question/answer text]
 
+// Player can select radio buttons for each question
 
+// Game ends when the time runs out
 
+// Player is presented with a splash screen displaying:
+    All Done!
+
+    Correct Answers total
+    Incorrect Answers total
+    Unanswered total
+
+    TOTAL === 10 [number of questions]
 
 
 
