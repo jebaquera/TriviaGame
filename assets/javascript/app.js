43d07191cba32d40083fd0d3209ca893d20a3b97
jebaquera <jebaquera@gmail.com>
2019-06-06 23:41:42 -0500

    final edits to layout and styles.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,153 +1,186 @@
-$( document ).ready(function() {
+$( document ).ready(function(){ 
+    $("#remaining-time").hide();
+    $("#start").on('click', trivia.startGame);
+    $(document).on('click', '.option', trivia.guessChecker);
+    
+})
 
-// this is for the on click function (button) that starts the game
-$("#startButton").on("click", function (){
+// the following variable holds all of the items needed for the trivia game quiz including the question and answer choice options. Answer value is index to the correct answer in the array.
+var trivia = {
+    correct: 0,
+    incorrect: 0,
+    unanswered: 0,
+    currentSet: 0,
+    timer: 40,
+    timerOn: false,
+    timerId : '',
+    questions: {
+        q1: "What was the name of the first Bond film?",
+        q2: "SPECTRE stands for Special Executive For Counter-Intelligence, Terrorism, Revenge and ... what?",
+        q3: "Which film featured London's Millenium Dome?",
+        q4: "What is the codename of Goldfinger's plan to rob Fort Knox?",
+        q5: "What does the 'oo' signify in 007?",
+        q6: "What is Moonraker?",
+        q7: "Who sang the title theme for DIAMONDS ARE FOREVER?",
+        q8: "What famous landmark does Grace Jones jump from in A VIEW TO A KILL?",
+        q9: "Which villain had a manservant called Nick Nack?",
+    },   
+    options: {
+        q1: ["Dr. No", "Casino Royale", "On Her Majesty's Secret Service"],
+        q2: ["Extinction", "Extortion", "Examination"],
+        q3: ["Quantum of Solace", "The World Is Not Enough", "A View To A Kill"],
+        q4: ["Hold The Fort", "Gold Run", "Operation Grand Slam"],
+        q5: ["The rank of commander", "A license to kill", "A license to carry firearms"],
+        q6: ["A space shuttle", "A laser telescope", "A plan to bomb the moon"],
+        q7: ["Rita Coolidge", "Nancy Sinatra", "Shirley Bassey"],
+        q8: ["Big Ben", "The Empire State Building", "The Eiffel Tower"],
+        q9: ["Francisco Scaramanga, The Man With The Golden Gun", "Elektra King, The World Is Not Enough", "Rosa Klebb, From Russia With Love"],
+    },
+    answers: {
+        q1: "Dr. No",
+        q2: "Extortion",
+        q3: "The World Is Not Enough",
+        q4: "Operation Grand Slam",
+        q5: "A license to kill",
+        q6: "A space shuttle",
+        q7: "Shirley Bassey",
+        q8: "The Eiffel Tower",
+        q9: "Francisco Scaramanga, The Man With The Golden Gun",
+    },
+    
+// Game start
+startGame: function(){
+    // restarting game results
+    trivia.currentSet = 0;
+    trivia.correct = 0;
+    trivia.incorrect = 0;
+    trivia.unanswered = 0;
+    clearInterval(trivia.timerId);
+    
+    // show game section
+    $('#game').show();
+    
+    // empty last results
+    $('#results').html('');
+    
+    // show timer
+    $('#timer').text(trivia.timer);
+    
+    // remove start button
+    $('#start').hide();
 
-// clicking the start button will trigger the questions div to display
-    $(".container").show();
-    console.log("Im here!");
+    $('#remaining-time').show();
+    
+    // ask first question
+    trivia.nextQuestion();   
+},
 
-    // $(this).hide();
-});
-
-// this sets the timer in seconds for how long the player has to complete the game
-var number = 30
-$("#timeRemaining").on("click", run);
-    // Add a decrement function to decrease the number of seconds
-    // Display the time remaining until time is up 
-    // Use the #timeRemaining div
-    // When the timer reaches 0 seconds, stop the function
-    // Alert the user that the time is up. Show an html message with "Good job!"
-    // Check the player choices for corrent and incorrect reponses and unanswered questions
-
-
-// this sets the message shown to the player once time is up
-var timeUpMessage = "Good job!";
-
-// Set the run function to specify the time interval for the decrement function to equal one second per decrement
-function run (){
-    counter = setInterval(decrement, 1000);
+// Loop through and display questions and options 
+nextQuestion : function(){
+    
+    // set timer to 10 seconds for each question
+    trivia.timer = 10;
+    $('#timer').removeClass('last-seconds');
+    $('#timer').text(trivia.timer);
+    
+    if(!trivia.timerOn){
+        trivia.timerId = setInterval(trivia.timerRunning, 1000);
 }
-
-    // Set the stop function
-
-
-
-// the following triviaGame object holds all of the items needed for the trivia game quiz including the question and answer choice options. Answer value is index to the correct answer in the array.
-var triviaGame = {
-    questionsText: [
-    {
-        question: "What was the name of the first Bond film?",
-        answerOptions: ["Dr. No", "Casino Royale", "On Her Majesty&rsquo;s Secret Service"],
-        id: "question-one",
-        answer: 0
-    }, {
-        question: "SPECTRE stands for Special Executive For Counter-Intelligence, Terrorism, Revenge and ... what?",
-        answerOptions: ["Extinction", "Extortion", "Examination"],
-        id: "question-two",
-        answer: 1
-    }, {
-        question: "Which film featured London&rsquo;s Millenium Dome?",
-        answerOptions: ["Quantum of Solace", "The World Is Not Enough", "A View To A Kill"],
-        id: "question-three",
-        answer: 1
-    }, {
-        question: "What is the codename of Goldfinger&rsquo;s plan to rob Fort Knox?",
-        answerOptions: ["Hold The Fort", "Gold Run", "Operation Grand Slam"],
-        id: "question-four",
-        answer: 2
-    }, {
-        question: "What does the &ldquo;oo&rdquo; signify in 007?",
-        answerOptions: ["The rank of commander", "A license to kill", "A license to carry firearms"],
-        id: "question-five",
-        answer: 1
-    }, {
-        question: "What is Moonraker?",
-        answerOptions: ["A space shuttle", "A laser telescope", "A plan to bomb the moon"],
-        id: "question-six",
-        answer: 0
-    }, {
-        question: "Who sang the title theme for <em>Diamonds Are Forever</em>?",
-        answerOptions: ["Rita Coolidge", "Nancy Sinatra", "Shirley Bassey"],
-        id: "question-seven",
-        answer: 2
-    }, {
-        question: "What famous landmark does Grace Jones jump from in <em>A View To A Kill</em>?",
-        answerOptions: ["Big Ben", "The Empire State Building", "The Eiffel Tower"],
-        id: "question-eight",
-        answer: 2
-    }, {
-        question: "Which villain had a manservant called Nick Nack?",
-        answerOptions: ["Francisco Scaramanga, <em>The Man With The Golden Gun</em>", "Elektra King, <em>The World Is Not Enough</em>", "Rosa Klebb, <em>From Russia With Love</em>"],
-        id: "question-nine",
-        answer: 0
-    }, {
-        question: "Alongside Bernard Lee and Dame Judi Dench, who else has played &ldquo;M&rdquo;?",
-        answerOptions: ["Pedro Armendariz", "Anthony Dawson", "Robert Brown"],
-        id: "question-ten",
-        answer: 2
+    
+// gets all the questions then indexes the current questions
+var questionContent = Object.values(trivia.questions)[trivia.currentSet];
+    $('#question').text(questionContent);
+   
+// an array of all the user options for the current question
+var questionOptions = Object.values(trivia.options)[trivia.currentSet];
+    
+// creates all the trivia guess options in the html
+$.each(questionOptions, function(index, key){
+    $('#options').append($('<button class="option btn btn-info btn-lg">' + key + '</button>'));
+})
+    
+},
+// method to decrement counter and count unanswered if timer runs out
+timerRunning : function(){
+    // if timer still has time left and there are still questions left to ask
+    if(trivia.timer > -1 && trivia.currentSet < Object.keys(trivia.questions).length){
+      $('#timer').text(trivia.timer);
+      trivia.timer--;
+        if(trivia.timer === 4){
+          $('#timer').addClass('last-seconds');
+        }
     }
-    ]}
+    // the time has run out and incremented question went unanswered, run result
+    else if(trivia.timer === -1){
+      trivia.unanswered++;
+      trivia.result = false;
+      clearInterval(trivia.timerId);
+      resultId = setTimeout(trivia.guessResult, 1000);
+      $('#results').html('<h3>Time is up! The answer was '+ Object.values(trivia.answers)[trivia.currentSet] +'</h3>');
+    }
+    // if all the questions have been shown end the game, show results
+    else if(trivia.currentSet === Object.keys(trivia.questions).length){
+      
+      // adds results of game (correct, incorrect, unanswered) to the page
+      $('#results')
+        .html('<h3>Well done!</h3>'+
+        '<p>Correct: '+ trivia.correct +'</p>'+
+        '<p>Incorrect: '+ trivia.incorrect +'</p>'+
+        '<p>Unanswered: '+ trivia.unanswered +'</p>'+
+        '<p>Play again!</p>');
+      
+      // hide game sction
+      $('#game').hide();
+      
+      // show start button to begin a new game
+      $('#start').show();
+    }
+    
+  },
+  // Checking for correct answers
+    guessChecker : function() {
+    
+    // timer ID for gameResult setTimeout
+    var resultId;
+    
+    // the answer to the current question being asked
+    var currentAnswer = Object.values(trivia.answers)[trivia.currentSet];
+    
+    // if the text of the option picked matches the answer of the current question, increment correct
+    if($(this).text() === currentAnswer){
+      // turn button green for correct
+      $(this).addClass('btn-success').removeClass('btn-info');
+      
+      trivia.correct++;
+      clearInterval(trivia.timerId);
+      resultId = setTimeout(trivia.guessResult, 1000);
+      $('#results').html('<h3>Correct Answer!</h3>');
+    }
+    // else the user picked the wrong option, increment incorrect
+        else {
+      // turn button clicked red for incorrect
+      $(this).addClass('btn-danger').removeClass('btn-info');
+      
+      trivia.incorrect++;
+      clearInterval(trivia.timerId);
+      resultId = setTimeout(trivia.guessResult, 1000);
+      $('#results').html('<h3>Try again! '+ currentAnswer +'</h3>');
+    }
+    
+  },
+  // method to remove previous question results and options
+  guessResult : function(){
+    
+    // increment to next question set
+    trivia.currentSet++;
+    
+    // remove the options and results
+    $('.option').remove();
+    $('#results h3').remove();
+    
+    // begin next question
+    trivia.nextQuestion();
+     
+  }
 
-
-    // The following results variables will be presented to the user once the game session is over
-        var totalScoreHTML = " ";
-        var toatlGuessed = [];
-        var correctAnswers = 0;
-        var incorrectAnswers = 0;
-        var notAnswered = 0;
-
-
-    // for loop will traverse the triviaGame object for each question and determine 
-    // if the player answers match the indices for the correct answers
-    // Increment the correctAnswers score by 1
-
-
-
-    // else run the questions at each index and checkForAnswer function
-    // this will provide update numbers for incorrect and notAnswered variables.
-
-
-    // Display the results of the above function in the gameResults div
-    // Build results using html and text strings
-    $(".gameResults").html("Correct Answers: " + correctAnswers + "<br />" + "Incorrect Answers: " + incorrectAnswers + "<br />" + "Unanswered Questions: " + notAnswered);
-
-
-
-
-
-
-
-
-
-
-
-
-// add a function with  onclick event for the finishedButton that will check the player answer choices
-// and stops the timer when "Finsihed" button is pressed
-$("#finishedButton").on("click", function() {
-	checkAnswers();
-	stop();
-	$("#playerEndMessage").html("Well done! Try again.");
-	})
-
-});
-
-
-//Beginning pseudocode
-// Game starts with background [display background art, logo art, instruction text] and Start screen [button]
-
-// When player clicks Start, the Main Text screen displays [Show titling, time remaining stopwatch, and question/answer text]
-
-// Player can select radio buttons for each question
-
-// Game ends when the time runs out
-
-// Player is presented with a splash screen displaying:
-//   All Done!
-
-//   Correct Answers total
-//   Incorrect Answers total
-//   Unanswered total
-
-//   TOTAL === 10 [number of questions]
+}
